4de395018f8687d8995d3b83bd92e4279b18f4cb
Fang Pengfei <pffang@vip.qq.com>
2019-05-11 18:24:32 +0800

    Fix dll export

--- libiconv-1.16/libcharset/include/localcharset.h
+++ libiconv-1.16/libcharset/include/localcharset.h
@@ -18,14 +18,6 @@
 #ifndef _LOCALCHARSET_H
 #define _LOCALCHARSET_H
 
-#if 0 && BUILDING_LIBCHARSET
-#define LIBCHARSET_DLL_EXPORTED __attribute__((__visibility__("default")))
-#elif defined _MSC_VER && BUILDING_LIBCHARSET
-#define LIBCHARSET_DLL_EXPORTED __declspec(dllexport)
-#else
-#define LIBCHARSET_DLL_EXPORTED
-#endif
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,7 +29,7 @@ extern "C" {
    The result must not be freed; it is statically allocated.
    If the canonical name cannot be determined, the result is a non-canonical
    name.  */
-extern LIBCHARSET_DLL_EXPORTED const char * locale_charset (void);
+extern const char * locale_charset (void);
 
 /* About GNU canonical names for character encodings:
 
